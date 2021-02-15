b9c92c36b8910b0ad875be4336792e686ec90883
Celina Junker <junkerc2@nku.edu>
2021-02-15 11:21:10 -0500

    hangman game beginning

---
+++ hangManGame/src/ch03_TipCalculator/HangManGame.java
@@ -0,0 +1,14 @@
+package ch03_TipCalculator;
+
+public class HangManGame {
+
+	public static void main(String[] args) {
+		//welcome to hangmkm an!
+		//you know the drill, I'll pick a word and you have to guess it within 7 tries or else the dude dies!!!
+		//basically an ASCII line of text with lots of spaces and carriage return
+		System.out.println("Welcome to Hangman! \nYou know the drill...I'll pick a word and \nyou have to guess it within 7 tries \nor else the dude dies!!!");
+		System.out.println(" +---+\n |   |\n O   |\n/|\\  |\n/ \\  |\n     |\n========");
+		System.out.println("bye");
+	}
+
+}
