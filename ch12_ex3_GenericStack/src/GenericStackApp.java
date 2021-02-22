55aa4390fa68be8259bfbee3081ebcd927e8e6e3
Celina Junker <junkerc2@nku.edu>
2021-02-22 16:51:08 -0500

    chapter 12 exercises

--- ch12_ex3_GenericStack/src/GenericStackApp.java
+++ ch12_ex3_GenericStack/src/GenericStackApp.java
@@ -1,7 +1,27 @@
 public class GenericStackApp {
 
     public static void main(String[] args) {
+    	Stack<String> stack = new Stack<>();
+    	
+    	stack.push("Apples");
+    	System.out.println("Push: apples");
+    	stack.push("Oranges");
+    	System.out.println("Push: oranges");
+    	stack.push("Bananas");
+    	System.out.println("Push: bananas");
+    	System.out.println("The stack contains " + stack.size() + " items.\n");
 
+    	//the stacks contains x items
+    	System.out.println("Peek: " + stack.peek());
+    	System.out.println("The stack contains " + stack.size() + " items.\n");
+    	
+    	
+    	System.out.println("pop: " + stack.pop());
+    	System.out.println("pop: " + stack.pop());
+    	System.out.println("pop: " + stack.pop());
+    	System.out.println("The stack contains " + stack.size() + " items.\n");
+    	
+    	System.out.println("bye");
     }
     
 }
--- ch12_ex3_GenericStack/src/Stack.java
+++ ch12_ex3_GenericStack/src/Stack.java
@@ -3,6 +3,23 @@ import java.util.LinkedList;
 public class Stack<E> {
 	private LinkedList<E> list = new LinkedList<>();
 	
-
-
+	public void push(E item) {
+		//ads to top, like addFirst()
+		list.addFirst(item);
+	}
+	
+	public E pop() {
+		//returns top elements and removes, removeFirst()
+		return list.removeFirst();
+	}
+	
+	public E peek(){
+		//does a peek
+		return list.peek();
+	}
+	
+	public int size(){
+		//returns size
+		return list.size();
+	}
 }
