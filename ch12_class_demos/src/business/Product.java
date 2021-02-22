f4dc27e26f83b87067216b12ff7408c721f2dab8
Celina Junker <junkerc2@nku.edu>
2021-02-22 15:27:04 -0500

    updated class demos

---
+++ ch12_class_demos/src/business/Product.java
@@ -0,0 +1,72 @@
+package business;
+
+//pg 217
+//1. Create class
+// Name must match perfectly
+public class Product implements Comparable {
+	// all physical products need to be classes in Java
+	//2. Set instance variables.
+	private String code;
+	private String description;
+	private double price;
+	
+	
+	//3. Generate constructors
+	//"fully loaded" constructor - not required
+	public Product(String code, String description, double price) {
+		super();
+		this.code = code;
+		this.description = description;
+		this.price = price;
+	}
+
+	//4. Setters and getters
+	public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	
+	
+	//5. Optional - generate a toString
+	@Override
+	public String toString() {
+		return "Product [code=" + code + ", description=" + description + ", price=" + price + "]";
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		//compare code to determine sort order
+		
+		Product p = (Product) o;
+		return this.code.compareTo(p.getCode());
+		
+	}
+	
+}
+
+
--- ch12_class_demos/src/ui/CollectionDemoApp.java
+++ ch12_class_demos/src/ui/CollectionDemoApp.java
@@ -1,6 +1,10 @@
 package ui;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import business.Product;
 
 public class CollectionDemoApp {
 
@@ -16,7 +20,73 @@ public class CollectionDemoApp {
 		
 		for (String s:names) {
 			System.out.println(s);
+			
 		}
+		System.out.println("\n");
+		names.add(3, "Sean");
+		
+		for (String s:names) {
+			System.out.println(s);
+		}
+		
+		System.out.println("Name at index one: " + names.get(1));
+		
+		System.out.println("names size = " +names.size());
+	
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println(i + names.get(i));
+		}
+	
+		System.out.println("no loops, just names:" +names);
+		
+		//p 391
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(87);
+		numbers.add(45);
+		numbers.add(62);
+		numbers.add(20);
+		numbers.add(11);
+		
+		System.out.println("numbers.contains 62? " + numbers.contains(62));
+		System.out.println("20 occurs at index " + numbers.indexOf(20));
+		System.out.println("1 occurs at index " + numbers.indexOf(1));
+		numbers.remove(new Integer(20));
+		numbers.remove(1);
+		System.out.println(numbers);
+		
+		//set is actually replacing values
+		numbers.set(1, 33);
+		System.out.println(numbers);
+		
+		numbers.sort(null);
+		System.out.println(numbers);
+	
+		// p. 409
+		Product p1 = new Product("java", "Murach's Java Programming", 59.50);
+		Product p2 = new Product("andr", "Murach's Android Programming", 57.50);
+		Product p3 = new Product("net", "Murach's .Net Programming", 58.50);
+		
+		Map<String, Product> productMap = new HashMap<>();
+		productMap.put(p1.getCode(), p1);
+		productMap.put(p2.getCode(), p2);
+		productMap.put(p3.getCode(), p3);
+		
+		System.out.println(productMap.containsKey("andr"));
+		System.out.println(productMap.containsKey("test"));
+		System.out.println(productMap.containsValue(p2));
+		System.out.println(productMap.get("andr"));
+		
+		//pg 411 - print entries
+		for (Map.Entry<String, Product> product: productMap.entrySet()) {
+			System.out.println("Key: "+ product.getKey()+ ", value: " + product.getValue());
+		}
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
