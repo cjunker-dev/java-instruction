e2a60da43549ffbfb776f6abd83e3f2227dcb65d
Celina Junker <junkerc2@nku.edu>
2021-02-22 11:26:23 -0500

    initial commit for Collections demo

---
+++ ch12_class_demos/src/ui/CollectionDemoApp.java
@@ -0,0 +1,17 @@
+package ui;
+
+import java.util.ArrayList;
+
+public class CollectionDemoApp {
+
+	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<>();
+		names.add("Celina");
+		names.add("Will");
+		names.add("Lance");
+		names.add("Daemon");
+		names.add("Tom");
+		names.add("Bryan");
+	}
+
+}
