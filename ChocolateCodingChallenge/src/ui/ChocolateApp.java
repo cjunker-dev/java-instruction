f2a4dfd37b8e7f12798399f00fcb13338b450170
Celina Junker <junkerc2@nku.edu>
2021-02-24 17:06:23 -0500

    final comments added

--- ChocolateCodingChallenge/src/ui/ChocolateApp.java
+++ ChocolateCodingChallenge/src/ui/ChocolateApp.java
@@ -1,6 +1,14 @@
 package ui;
 
 public class ChocolateApp {
+	
+	/*
+	 * We want make a package of goal kilos of chocolate. 
+	 * We have small bars (1 kilo each) and big bars (5 kilos each). 
+	 * Return the number of small bars to use, 
+	 * assuming we always use big bars before small bars. 
+	 * Return -1 if it can't be done.
+	 */
 
 	public static void main(String[] args) {
 		System.out.println(makeChocolate(4,1,9)); //return 4
